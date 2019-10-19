5dd9aea02522c6ad4f8697b807b87e336fa21bc4
Bairam Batirov <hamraev.bairm@gmail.com>
2019-10-19 14:23:26 +0200

    add sleepQuality func

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,6 +18,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -42,15 +43,24 @@ class SleepTrackerViewModel(
         nights -> formatNights(nights, application.resources)
     }
 
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
+
     init {
         initializeTonigh()
     }
 
-  fun initializeTonigh(){
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
+    }
+
+    fun initializeTonigh(){
       uiScope.launch {
           tonight.value = getTonightFromDatabase()
       }
-  }
+     }
 
     private suspend fun getTonightFromDatabase() : SleepNight?{
         return withContext(Dispatchers.IO){
@@ -81,6 +91,7 @@ class SleepTrackerViewModel(
             val oldNight = tonight.value?:return@launch
             oldNight.endTimeMilli = System.currentTimeMillis()
             update(oldNight)
+            _navigateToSleepQuality.value = oldNight
         }
     }
 
